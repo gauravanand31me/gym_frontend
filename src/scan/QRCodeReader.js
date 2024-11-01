a5cebbddad7c24b34cd39f6565d33b1adff24ce2
Sourav Satyam <sourav.satyamsingh@gmail.com>
2024-11-02 01:06:21 +0530

    Added scanner file

--- src/scan/QRCodeReader.js
+++ src/scan/QRCodeReader.js
@@ -5,35 +5,29 @@ import axios from "axios";
 import "./Scanner.css";
 
 const QRCodeReader = () => {
-    // State to hold the scanned result and messages
     const [scanResult, setScanResult] = useState(null);
     const [messages, setMessages] = useState([]);
 
     const handleOnScan = async (result) => {
-        // Check if a result is obtained
         if (result) {
-            const bookingId = result[0].rawValue; // Assuming this is the booking ID
+            const bookingId = result[0].rawValue;
 
             try {
                 const response = await axios.get(bookingId, {
                     headers: { 'auth': document.cookie.replace(/(?:(?:^|.*;\s*)auth\s*=\s*([^;]*).*$)|^.*$/, "$1") }
                 });
-                
-                // Set the response messages
-                setMessages([response.data]); // Assuming the server response is a message
+
+                setMessages([response.data.message || response.data]);
             } catch (error) {
-                // Handle errors and display the error message
                 const errorMsg = error.response ? error.response.data.message : 'An error occurred';
                 setMessages([errorMsg]);
             }
 
-            // Update the state with the scanned result
             setScanResult(bookingId);
         }
     };
 
     const handleNewScan = () => {
-        // Reset the scan result and messages for a new scan
         setScanResult(null);
         setMessages([]);
     };
@@ -43,11 +37,7 @@ const QRCodeReader = () => {
             <Header />
             <ReactScan onScan={handleOnScan} />
             <div className="scan-result">
-                {/* Display the scanned JSON result here */}
-                {scanResult && (
-                    <pre>{JSON.stringify(scanResult, null, 2)}</pre> // Pretty-print JSON with indentation
-                )}
-                {/* Display messages */}
+                {scanResult && <pre>{JSON.stringify(scanResult, null, 2)}</pre>}
                 {messages.length > 0 && (
                     <div className="messages">
                         {messages.map((msg, index) => (
@@ -56,7 +46,6 @@ const QRCodeReader = () => {
                     </div>
                 )}
             </div>
-            {/* Button for a new scan */}
             <button onClick={handleNewScan} className="new-scan-button">
                 New Scan
             </button>
