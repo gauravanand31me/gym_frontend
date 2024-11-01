1fd2627e6ace8be260c13e72785ffea27a36acda
Sourav Satyam <sourav.satyamsingh@gmail.com>
2024-11-01 12:51:25 +0530

    Added axios

--- src/scan/QRCodeReader.js
+++ src/scan/QRCodeReader.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Header from '../components/Header';
 import ReactScan from '../components/Scanner';
+import axios from "axios";
 
 const QRCodeReader = () => {
     // State to hold the scanned result
